ae55edbf02e46145b22b0741fb8f1ac5ef0ecf52
Patrick Holberg Hesselberg <phh@peytz.dk>
2015-11-23 12:14:19 +0000

    wrap fields

--- views/admin/metabox-translations-post/trapp.php
+++ views/admin/metabox-translations-post/trapp.php
@@ -1,7 +1,29 @@
-<label for"trapp-comment"><?php _e('Comment'); ?></label>
-<textarea name="trapp_comment" id="trapp-comment"></textarea>
+<div class="form-wrap">
 
-<label for"trapp-deadline"><?php _e('Deadline'); ?></label>
-<input type="text" value="<?php echo date('Y-m-d H:i:s', current_time('timestamp')); ?>">
+    <div class="form-field">
+        <label for="trapp-comment">
+            <strong><?php _e('Comment:'); ?></strong>
+        </label>
+        <textarea name="trapp_comment" id="trapp-comment"></textarea>
+    </div>
 
-<?php submit_button(__('Send to TRAPP'), 'primary', 'send_to_trapp'); ?>
+    <div class="form-field">
+        <label for="trapp-deadline">
+            <strong><?php _e('Deadline:'); ?></strong>
+        </label>
+        <input type="text" id="trapp-deadline" value="<?php echo date('Y-m-d', current_time('timestamp')); ?>">
+    </div>
+
+    <div class="form-field">
+        <label for="trapp-start">
+            <input type="checkbox" id="trapp-start" value="1">
+            <strong><?php _e('Start translation?'); ?></strong>
+        </label>
+        <p class="description">
+            <?php printf(__('This will set the TRAPP status to %s instead of %s.'), '<code>Missing</code>', '<code>On Hold</code>'); ?>
+        </p>
+    </div>
+
+    <?php submit_button(__('Send to TRAPP'), 'primary large', 'send_to_trapp'); ?>
+
+</div>
