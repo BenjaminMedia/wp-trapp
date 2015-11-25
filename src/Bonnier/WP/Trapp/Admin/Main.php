78ec896a3713fbd5c3434e0a3e2bdea0de9e33d4
Patrick Holberg Hesselberg <phh@peytz.dk>
2015-11-25 15:23:37 +0000

    adds edit_post listener

--- src/Bonnier/WP/Trapp/Admin/Main.php
+++ src/Bonnier/WP/Trapp/Admin/Main.php
@@ -13,6 +13,7 @@ class Main
     {
         add_action('pll_init', [__CLASS__, 'polylangInit']);
         add_action('bp_pll_init', [__CLASS__, 'bpPllInit']);
+        add_action('edit_post', [__CLASS__, 'editPost']);
     }
 
     /**
@@ -33,6 +34,49 @@ class Main
     public static function bpPllInit()
     {
         add_action('do_meta_boxes', [__CLASS__, 'polylangMetaBox'], 10, 2);
+    }
+
+    /**
+     * Hook listener for edit_post.
+     *
+     * @param int $postId Post id of the edited post.
+     *
+     * @return void
+     */
+    public static function editPost($postId)
+    {
+        if (!isset($_POST['send_to_trapp'])) {
+            return;
+        }
+
+        // Exclude auto-draft
+        if (get_post_status($postId) == 'auto-draft') {
+            return;
+        }
+
+        // Only specific post types
+        $post_type = 'review';
+        $post_types = [$post_type]; // TODO Filter to include more post_types
+
+        if (!in_array($post_type, $post_types)) {
+            return;
+        }
+
+        /**
+         * Fired once a post with a TRAPP action has been saved.
+         *
+         * Specific to the saved post type.
+         *
+         * @param int     $postId  Post ID.
+         */
+        do_action('save_trapp_' . $post_type, $postId);
+
+        /**
+         * Fired once a post with a TRAPP action has been saved.
+         *
+         * @param int     $postId  Post ID.
+         */
+        do_action('save_trapp', $postId);
     }
 
     /**
