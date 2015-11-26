9e300be6ee3f2418bc9cc66ad09111bd0802c973
Patrick Holberg Hesselberg <phh@peytz.dk>
2015-11-26 12:55:43 +0000

    use coreBootstrap to set default values to the trapp_service object

--- src/Bonnier/WP/Trapp/Core/Bootstrap.php
+++ src/Bonnier/WP/Trapp/Core/Bootstrap.php
@@ -13,5 +13,30 @@ class Bootstrap
             $admin = new Admin\Main;
             $admin->bootstrap();
         }
+
+        $this->coreBootstrap();
+    }
+
+    public function coreBootstrap() {
+        add_filter('bp_trapp_service_development', '__return_false');
+        add_filter('bp_trapp_service_development', '__return_true'); // TODO This should come from the project or from a small plugin
+        add_filter('bp_trapp_service_username', [__CLASS__, 'serviceUser']);
+        add_filter('bp_trapp_service_secret', [__CLASS__, 'serviceSecret']);
+    }
+
+    public static function serviceUser($user) {
+        if (defined('WA_TRAPP_USERNAME')) {
+            return WA_TRAPP_USERNAME;
+        }
+
+        return $user;
+    }
+
+    public static function serviceSecret($secret) {
+        if (defined('WA_TRAPP_SECRET')) {
+            return WA_TRAPP_SECRET;
+        }
+
+        return $secret;
     }
 }
