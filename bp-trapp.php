909f79689a1011cdaa0a90f47fcd97464db0e017
Patrick Holberg Hesselberg <phh@peytz.dk>
2016-01-25 14:49:53 +0000

    update now that readme is updated

--- bp-trapp.php
+++ bp-trapp.php
@@ -5,7 +5,7 @@
  * Plugin Name:       BP WP TRAPP
  * Plugin URI:        https://github.com/BenjaminMedia/wp-trapp
  * Description:       Send content to the TRAPP translation service.
- * Version:           0.1.2
+ * Version:           0.2.0
  * Text Domain:       bp-trapp
  * Domain Path:       /languages
  */
